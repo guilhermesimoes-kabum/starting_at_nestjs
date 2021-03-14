140fadb6541e01705f12eb641f17431e9787c35e
Guilherme <trintaeoitogc@gmail.com>
2021-03-14 13:02:43 -0300

    feat: adding controller to admin

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
 import { CatsService } from './cats/cats.service';
+import { AdminController } from './admin/admin.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, CatsController],
+  controllers: [AppController, CatsController, AdminController],
   providers: [AppService, CatsService],
 })
 export class AppModule {}
